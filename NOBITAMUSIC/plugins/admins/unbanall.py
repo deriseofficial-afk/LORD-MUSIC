7bc1b4b67ac6f37e0e81324fdbc23f9a4bbb29b9
abhaysinghchauhan001 <chauhanabhay206243@gmail.com>
2025-04-22 14:16:05 +0530

    Update unbanall.py

--- NOBITAMUSIC/plugins/admins/unbanall.py
+++ NOBITAMUSIC/plugins/admins/unbanall.py
@@ -2,7 +2,7 @@ from NOBITAMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from NOBITAMUSIC.utils.Shukla_ban import admin_filter
+from NOBITAMUSIC.utils.NOBITA_ban import admin_filter
 
 BOT_ID = "6824607634"
 
